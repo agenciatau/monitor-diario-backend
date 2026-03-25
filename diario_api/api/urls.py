a21d772236aaab89517a84657404ce0b7dc63f3e
Victor Gois <victorgois18@gmail.com>
2026-03-25 20:33:02 -0300

    Remove django api files

--- diario_api/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ChatView, UploadView, AnalyzeView
-
-urlpatterns = [
-    path("chat/", ChatView.as_view(), name="chat"),
-    path("upload/", UploadView.as_view(), name="upload"),
-    path("analyze/", AnalyzeView.as_view(), name="analyze"),
-]
